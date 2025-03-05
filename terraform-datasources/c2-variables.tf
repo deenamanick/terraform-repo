9bcba2897ddb684371c7431487f159bb7bdbcad1
“deenamanick” <deenamanick@gmail.com>
2025-03-05 18:03:29 +0530

    adding terraform datasources repo

---
+++ terraform-datasources/c2-variables.tf
@@ -0,0 +1,43 @@
+# Input Variables
+variable "aws_region" {
+  description = "Region in which AWS Resources to be created"
+  type        = string
+  default     = "us-east-1"
+}
+
+/* 
+# Commented as we are going to get AMI ID from Datasource aws_ami
+variable "ec2_ami_id" {
+  description = "AMI ID"
+  type = string  
+  default = "ami-0915bcb5fa77e4892"
+}
+*/
+
+variable "ec2_instance_type" {
+  description = "EC2 Instance Type"
+  type = list(object({
+    role       = string
+    type       = string
+    private_ip = string
+  }))
+  default = [
+    { role = "master", type = "t3.micro", private_ip = "10.0.1.45" },
+    { role = "worker1", type = "t3.micro", private_ip = "10.0.1.46" },
+    { role = "worker2", type = "t3.micro", private_ip = "10.0.1.47" }
+  ]
+}
+
+variable "access_key" {
+  description = "Access key to AWS console"
+}
+variable "secret_key" {
+  description = "Secret key to AWS console"
+}
+variable "region" {
+  description = "AWS region"
+}
+
+# variable "worker_private_ips" {
+#   description = "worker private ip"
+# }
---
+++ terraform-datasources/c5-outputs.tf
@@ -0,0 +1,39 @@
+# Define Output Values
+
+# Attribute Reference
+output "ec2_instance_publicip" {
+  description = "EC2 Instance Public IP"
+  # value       = aws_instance.my-ec2-vm["each.Key"].public_ip
+  value = {
+    for key, instance in aws_instance.my-ec2-vm : key => instance.public_ip
+  }
+}
+
+# Attribute Reference - Create Public DNS URL 
+output "ec2_publicdns" {
+  description = "Public DNS URL of an EC2 Instance"
+  # value       = aws_instance.my-ec2-vm["each.key"].public_dns
+  value = {
+    for key, instance in aws_instance.my-ec2-vm : key => instance.public_ip
+  }
+}
+
+
+output "ami_id" {
+  value = data.aws_ami.amzlinux.id
+
+}
+
+# output "cidr_blocks_debug" {
+#   value = local.limited_cidr_blocks
+# }
+
+# # Output worker IPs for debugging
+# output "worker_private_ips" {
+#   value = local.worker_private_ips
+# }
+
+output "private_key_pem" {
+  value     = tls_private_key.example.private_key_pem
+  sensitive = true # Hides the private key from logs
+}
